f41cb98645e8a415d9a1b8d4521979c3b8802855
Ashish <ashes7882@gmail.com>
2021-02-03 16:22:44 -0500

    added sql scripts and WAR file

---
+++ Scripts/create_airlineData.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `mydb`.`airlines` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+INSERT INTO `mydb`.`airlines` (`id`, `name`) VALUES ('1', 'Air Canada');
+INSERT INTO `mydb`.`airlines` (`id`, `name`) VALUES ('2', 'Air India');
+INSERT INTO `mydb`.`airlines` (`id`, `name`) VALUES ('3', 'WestJet');
+INSERT INTO `mydb`.`airlines` (`id`, `name`) VALUES ('4', 'Air France');
+INSERT INTO `mydb`.`airlines` (`id`, `name`) VALUES ('5', 'British Airways');
+INSERT INTO `mydb`.`airlines` (`id`, `name`) VALUES ('6', 'United Airlines');
---
+++ Scripts/create_citiesData.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `mydb`.`cities` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `cities` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`));
+
+INSERT INTO `mydb`.`cities` (`ID`, `cities`) VALUES ('1', 'Toronto');
+INSERT INTO `mydb`.`cities` (`ID`, `cities`) VALUES ('2', 'Delhi');
+INSERT INTO `mydb`.`cities` (`ID`, `cities`) VALUES ('3', 'London');
+INSERT INTO `mydb`.`cities` (`ID`, `cities`) VALUES ('4', 'New York');
+INSERT INTO `mydb`.`cities` (`ID`, `cities`) VALUES ('5', 'Mumbai');
+INSERT INTO `mydb`.`cities` (`ID`, `cities`) VALUES ('6', 'Paris');
